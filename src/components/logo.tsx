4498674ee332ac442a521575ddbc8abaa5e14cc5
Syed Ahmad Kumail Zaidi <kumail.zaidi708@gmail.com>
2025-09-21 09:41:03 +0000

    ab jo quick summarize ka icon h usko bhi same icon kr do

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -2,11 +2,17 @@ export function Logo() {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
       viewBox="0 0 24 24"
-      fill="currentColor"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
       className="h-10 w-10 text-primary"
     >
-      <path d="M7 21l3-9L7 3h10l-3 9 3 9H7z" />
+      <polygon points="13 2 3 14 12 14 11 22 21 10 12 10 13 2" />
     </svg>
   );
 }
