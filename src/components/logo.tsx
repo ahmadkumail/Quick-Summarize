1169ce9f520ad4c48a7a1ecfdeca04380f03ed7d
Syed Ahmad Kumail Zaidi <kumail.zaidi708@gmail.com>
2025-09-21 08:51:12 +0000

    yr iska icon to thora modren kro flash hi ho or is me AI ka factor bhi a

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -4,7 +4,7 @@ export function Logo() {
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 24 24"
       fill="currentColor"
-      className="h-10 w-10 text-primary animate-pulse-slow"
+      className="h-10 w-10 text-primary"
     >
       <path d="M13 2L3 14h9l-1 8 10-12h-9l1-8z" />
     </svg>
