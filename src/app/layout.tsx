b4259c3d27ff9065479f6872242893a2f048e2b5
Syed Ahmad Kumail Zaidi <kumail.zaidi708@gmail.com>
2025-09-27 09:18:14 +0000

    Copy and paste this code between the <head></head> tags on each page of

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from 'next';
+import Script from 'next/script';
 import { Toaster } from '@/components/ui/toaster';
 import './globals.css';
 import { GeistSans } from 'geist/font/sans';
@@ -37,6 +38,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={`${GeistSans.variable} ${GeistMono.variable}`} suppressHydrationWarning>
+      <head>
+        <Script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5530405178327113"
+          crossOrigin="anonymous"
+          strategy="afterInteractive"
+        />
+      </head>
       <body>
         {/* Google Analytics */}
         <Script
