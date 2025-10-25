6ad8b68000b98435c439e1ed6273f5ed575d4a17
Syed Ahmad Kumail Zaidi <kumail.zaidi708@gmail.com>
2025-10-25 14:00:10 +0000

    Why Choose Quick Summarize? iska try it now ko functional kro

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -32,7 +32,7 @@ export default function Home() {
       <Header />
       <main className="flex-1 w-full">
         <div className="w-full max-w-7xl mx-auto p-4 md:p-6 grid gap-8 grid-cols-1 lg:grid-cols-2 lg:gap-12 items-start">
-          <div className="flex flex-col gap-6 md:sticky top-[80px]">
+          <div id="summarizer-form" className="flex flex-col gap-6 md:sticky top-[80px]">
             <SummarizerForm
               setSummary={setSummary}
               setIsLoading={setIsLoading}
